abe0994fcd14ffb8c6f17aba8c0f1db1e445e088
Andreas Kingbäck <andki234@gmail.com>
2024-03-24 00:55:44 +0100

    new ttt, reading from socket

---
+++ pyscripts/logiview/logiview_pm.py
@@ -0,0 +1,286 @@
+# LogiView_pm (Process Monitor)
+# =============================
+#
+# Description:
+# -----------
+# This script continuously monitors specified Python scripts to ensure they're running. If any of the
+# monitored scripts are found not to be running, this script will start them with the predefined arguments.
+
+# Each script being monitored can have a unique process title set using the setproctitle module. This
+# title is used by the monitor script to identify the process and check its running status.
+#
+# If a monitored script crashes or stops for any reason, the monitor will detect its absence within a
+# minute and will attempt to restart it.
+#
+# The scripts to be monitored, their unique process titles, and their associated arguments are defined
+# in the `scripts_to_monitor` dictionary.
+#
+# Note: Ensure that the titles specified in `scripts_to_monitor` match the titles set in each respective
+# script using the setproctitle module.
+#
+# Usage:
+# ------
+# To use the script, you need to provide the MySQL server's password.
+#
+# Run the script with the following format:
+#     python3 logiview_pm.py -p <PASSWORD>
+#
+# Where:
+#     <PASSWORD> is the MySQL password.
+#
+# Key Features:
+# -------------
+# 1. Continuously monitors specified Python scripts to check if they're running.
+# 2. Restarts any monitored script that stops or crashes.
+# 3. Easy to add or remove scripts from the monitoring process by simply updating the dictionary.
+# 4. Uses unique process titles for each script, set using the setproctitle module.
+# 5. Ensures titles in the `scripts_to_monitor` dictionary match with the titles set in the monitored scripts.
+#
+# Additional setup for scripts requiring privileged ports:
+# --------------------------------------------------------
+# Some scripts might need to bind to privileged ports (ports below 1024).
+# Instead of running these scripts as root, you can use `authbind` to grant them access.
+#
+# Setting up authbind:
+# 1. Install authbind:
+#    sudo apt-get install authbind
+#
+# 2. Allow a specific user to bind to a specific port (e.g., port 102 for seimens logo8):
+#    sudo touch /etc/authbind/byport/102
+#    sudo chown user_name /etc/authbind/byport/102
+#    sudo chmod 755 /etc/authbind/byport/102
+#
+# 3. Use authbind to run your script:
+#    authbind python3 your_script.py
+#
+# Replace `user_name` with the name of the user that will run the script (e.g., "pi").
+
+# Standard library imports
+import argparse          # Parser for command-line options and arguments
+import io                # Core tools for working with streams
+import logging           # Logging library for Python
+import logging.handlers  # Additional handlers for the logging module
+import subprocess        # To spawn new processes, connect to their input/output/error pipes
+import sys               # Access to Python interpreter variables
+import time              # Time-related functions
+from datetime import datetime   # Date/Time-related functions
+
+# Third-party imports
+import setproctitle                 # Allows customization of the process title
+from pushbullet import Pushbullet   # Using Pushbullet to send notifications to phone
+
+# Setting up process title for the monitor script
+setproctitle.setproctitle("logiview_pm")
+
+# Set to appropriate value to for logging level
+LOGGING_LEVEL = logging.INFO
+USE_PUSHBULLET = True
+
+
+class PushBullet:
+    def __init__(self, enabled, logger, apikey, name):
+        try:
+            self.enabled = enabled  # True to enable messages to be sent
+            self.initialized = False  # Init status
+            self.name = name  # Name to use in reports
+
+            # Create Pushbullet object
+            if enabled:
+                self.pushbullet = Pushbullet(apikey)
+        except Exception as e:
+            logger.error(f"Error during pushbullet object initialization: {e}")
+        else:
+            self.initialized = True
+
+    def push(self, level, str):
+        if "self.pushbullet" in locals():
+            try:
+                self.timestamp = datetime.now().strftime("%y-%m-%d %H:%M")  # Time stamp
+                self.pushbullet.push_note(f"{level}: {self.name}", f"[{self.timestamp}] {str}")
+            except Exception as e:
+                logger.error(f"Pushbullet error: {e}")
+        else:
+            pass
+
+    def send_info(self, str):
+        self.push("INFO", str)
+
+    def send_warning(self, str):
+        self.push("WARNING", str)
+
+    def send_error(self, str):
+        self.push("ERROR", str)
+
+
+class LogiviewPMserver:
+    def __init__(self):
+        try:
+            # Init status
+            self.initialized = False
+
+            # Create logger
+            self.logger = self.setup_logging(LOGGING_LEVEL)
+
+            # Parse command line arguments
+            parser = argparse.ArgumentParser(description="Logiview process monitor script")
+            parser.add_argument("--host", required=False, help="MySQL server ip address", default="192.168.0.240")
+            parser.add_argument("-u", "--user", required=False, help="MySQL server username", default="pi")
+            parser.add_argument("-p", "--password", required=True, help="MySQL password")
+            parser.add_argument("-a", "--apikey", required=True, help="API-Key for pushbullet")
+            self.args = parser.parse_args()
+            self.logger.info("Parsed command-line arguments successfully!")
+
+            # Create Pushbullet object
+            self.pushbullet = PushBullet(USE_PUSHBULLET, logger, self.args.apikey, "Logiview PM")
+
+            self.pushbullet.send_info("logiview_pm.py started")
+        except argparse.ArgumentError as e:
+            self.logger.error(f"Error parsing command-line arguments: {e}")
+            self.pushbullet.error(f"Error parsing command-line arguments: {e}")
+        except Exception as e:
+            self.logger.error(f"Error during initialization: {e}")
+            self.pushbullet.error(f"Error during initialization: {e}")
+        else:
+            self.initialized = True
+
+    def setup_logging(self, logging_level=logging.WARNING):
+        try:
+            # Setting up the logging
+            logger = logging.getLogger('logiview_pm')
+            logger.setLevel(logging_level)
+
+            # For syslog
+            syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+            syslog_format = logging.Formatter('%(name)s[%(process)d]: %(levelname)s - %(message)s')
+            syslog_handler.setFormatter(syslog_format)
+            logger.addHandler(syslog_handler)
+
+            # For console
+            console_handler = logging.StreamHandler()
+            console_format = logging.Formatter('%(levelname)s - %(message)s')
+            console_handler.setFormatter(console_format)
+            logger.addHandler(console_handler)
+
+            # Create an in-memory text stream to capture stderr
+            captured_output = io.StringIO()
+            self.original_stderr = sys.stderr
+            sys.stderr = captured_output  # Redirect stderr to captured_output
+
+            return logger
+        except Exception as e:
+            return None
+
+    def mask_password(self, args):
+        # Mask the password in the arguments list.
+        # Returns a new list with the password replaced by '<hidden>'.
+        masked_args = args[:]
+        try:
+            password_index = masked_args.index("--password") + 1
+            if password_index < len(masked_args):
+                masked_args[password_index] = "<hidden>"
+        except ValueError:
+            return ' '.join(self.args[:])  # "--password" not found in args
+        return ' '.join(masked_args)
+
+    def check_and_start(self, scripts_with_titles):
+        for script, (title, args, use_authbind, use_setsid) in scripts_with_titles.items():
+            # Check if the script is running using pgrep
+            try:
+                subprocess.check_output(["pgrep", "-f", title])
+                self.logger.info(f"{title} is running.")
+            except subprocess.CalledProcessError:
+                # The script is not running, so start it with its associated arguments.
+                self.logger.warning(f"{title} is not running. Starting it...")
+                cmd = ["authbind", "python3", script] + args if use_authbind else ["python3", script] + args
+                print(f"{cmd}\r\n")
+                # If use_setsid is True, then use setsid to run the process in its own session
+                cmd = ['setsid'] + cmd if use_setsid else cmd
+                subprocess.Popen(cmd)
+                self.logger.info(f"{title} has been started as: {self.mask_password(cmd)}")
+
+
+    def main_loop(self):
+        try:
+            scripts_to_monitor = {
+                "/home/pi/logiview/logiview_sds.py": (
+                    "logiview_sds",
+                    ["--host", "192.168.0.240", "--user", "pi", "--password", self.args.password],
+                    True,  # use_authbind for this script
+                    True  # do not use_setsid for this script
+                ),
+                "/home/pi/logiview/logiview_logo8.py": (
+                    "logiview_logo8",
+                    ["--host", "192.168.0.240", "--user", "pi", "--password",
+                        self.args.password, "--apikey", self.args.apikey],
+                    True,  # use_authbind for this script
+                    True  # do not use_setsid for this script
+                ),
+                "/home/pi/logiview/logiview_pds.py": (
+                    "logiview_pds",
+                    ["--host", "192.168.0.240", "--user", "pi", "--password", self.args.password],
+                    False,  # do not use_authbind for this script
+                    True  # do not use_setsid for this script
+                ),
+                "/home/pi/logiview/logiview_ttt.py": (
+                    "logiview_ttt",
+                    ["--host", "192.168.0.240", "--user", "pi", "--password",
+                        self.args.password, "--apikey", self.args.apikey],
+                    True,  # use_authbind for this script
+                    True  # do not use_setsid for this script
+                ),
+                "/home/pi/logiview/logiview_tpds.py": (
+                    "logiview_tpds",
+                    ["--host", "192.168.0.240", "--user", "pi", "--password", self.args.password],
+                    False,  # do not use_authbind for this script
+                    True  # do not use_setsid for this script
+                ),
+                 "/home/pi/logiview/logiview_envds.py": (
+                    "logiview_envds",
+                    ["--host", "192.168.0.240", "--user", "pi", "--password", self.args.password],
+                    False,  # do not use_authbind for this script
+                    True  # do not use_setsid for this script
+                ),
+                # ... add more scripts with their titles, arguments, authbind necessity, and setsid usage as needed
+            }
+
+            while True:
+                self.timestamp = datetime.now().strftime("%y-%m-%d %H:%M")  # Update timestamp
+                self.check_and_start(scripts_to_monitor)
+                time.sleep(28)  # Sleep for 28 seconds
+
+        except KeyboardInterrupt:
+            self.logger.info("Received a keyboard interrupt. Shutting down gracefully...")
+            if USE_PUSHBULLET:
+                self.pushbullet.push_note("INFO: LogiView PM",
+                                          f"[{self.timestamp}] Received a keyboard interrupt. Shutting down gracefully...")
+            sys.exit(0)
+        except SystemExit as e:
+            sys.stderr = self.original_stderr  # Reset stderr to its original value
+            error_message = self.captured_output.getvalue().strip()
+            if error_message:  # Check if there's an error message to log
+                self.logger.error(f"Command line arguments error: {error_message}")
+                if USE_PUSHBULLET:
+                    self.pushbullet.push_note("ERROR: LogiView PM",
+                                              f"[{self.timestamp}] Command line arguments error: {error_message}")
+            sys.exit(1)
+        except Exception as e:
+            self.logger.error(f"An unexpected error occurred: {e}")
+            if USE_PUSHBULLET:
+                self.pushbullet.push_note("ERROR: LogiView PM",
+                                          f"[{self.timestamp}] An unexpected error occurred: {e}")
+            sys.exit(1)
+
+
+def main():
+    logiview_server = LogiviewPMserver()   # Create PM-Server
+    if logiview_server.initialized:
+        logiview_server.main_loop()            # if all ok then execute main loop
+    else:
+        logiview_server.pushbullet.push_note(
+            "ERROR: LogiView PM", f"[{logiview_server.timestamp}] Initialize failed. Server not started!")
+        logiview_server.logger.error("Initialize failed. Server not started!")
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger('logiview_pm')
+    main()
---
+++ pyscripts/logiview/logiview_ttt.py
@@ -0,0 +1,281 @@
+# LogiView Logo8 Transfer Tank Temperatures
+# =================================================
+#
+# Description:
+# -----------
+# This Python script is designed to read temperature data from a TCP port and store it in a MySQL database.
+# It continuously monitors the connected UART device and extracts temperature data, which is then associated with
+# sensor information stored in the MySQL database.
+#
+# Usage:
+# ------
+# To use the script, you need to provide necessary command-line arguments such as the MySQL server's
+# IP address, username, and password. Proper error reporting mechanisms are in place to guide the user
+# in case of incorrect arguments or encountered issues.
+#
+# Run the script with the following format:
+#     python logiview_tth.py --host <MYSQL_SERVER_IP> -u <USERNAME> -p <PASSWORD>
+#
+# Where:
+#     <MYSQL_SERVER_IP> is the MySQL server's IP address.
+#     <USERNAME> is the MySQL server username.
+#     <PASSWORD> is the MySQL password.
+#
+# Key Features:
+# -------------
+# - Connects to a MySQL database to store temperature data.
+# - Reads temperature data from a UART
+# - Logs temperature readings along with timestamps in the database.
+# - Gracefully handles errors, including MySQL connection issues and UART communication timeouts.
+# - Can be customized to include additional sensors or data sources.
+#
+
+# Import necessary libraries
+import argparse                 # For parsing command-line options and arguments
+import io                       # For working with streams
+import socket                   # For network-related functions
+import logging                  # For logging messages
+import logging.handlers         # For additional logging handlers
+import sys                      # For accessing Python interpreter variables
+import time                     # For time-related functions
+import json                     # For JSON encoding/decoding
+from datetime import datetime   # Date/Time-related functions
+
+# Third-party imports
+from mysql.connector import errorcode   # Specific error codes from MySQL connector
+import mysql.connector                  # For MySQL database interaction
+import setproctitle                     # For customizing process title
+from pushbullet import Pushbullet       # Using Pushbullet to send notifications to phone
+
+# Set to appropriate value to enable/disabled logging
+LOGGING_LEVEL = logging.WARNING
+USE_PUSHBULLET = False
+
+
+class LogiviewTTHserver:
+    def __init__(self):
+        try:
+            # Init status
+            self.initialized = False
+            
+            # Create logger
+            self.logger = self.setup_logging(logging_level = LOGGING_LEVEL)
+            self.logger.debug("Logger initialized successfully")
+            
+            # Timestamp
+            self.timestamp = datetime.now().strftime("%y-%m-%d %H:%M")
+
+            # Parse command line arguments
+            parser = argparse.ArgumentParser(description="Logo8 server script")
+            parser.add_argument("-mh", "--host", required=False, help="MySQL server ip address", default="192.168.0.240")
+            parser.add_argument("-u", "--user", required=False, help="MySQL server username", default="pi")
+            parser.add_argument("-dh", "--datahost", required=False, help="Data host ip address", default="192.168.162")
+            parser.add_argument("-dp", "--dataport", required=False, help="Data host port", default = 18999)    
+            parser.add_argument("-p", "--password", required=True, help="MySQL password")
+            parser.add_argument("-a", "--apikey", required=True, help="API-Key for pushbullet")
+            parser.add_argument("-s", "--snap7-lib", default=None, help="Path to Snap7 library")
+                
+            try:
+                args = parser.parse_args()
+            except SystemExit:
+                error_message = sys.stderr.getvalue().strip()
+                self.logger.error(f"Error during parsing: {error_message}")
+                sys.exit(1)
+                   
+            self.logger.debug("Parsed command-line arguments successfully!")
+            
+            # Create pushbullet
+            if USE_PUSHBULLET:
+                self.pushbullet = Pushbullet(args.apikey)
+                self.pushbullet.push_note("INFO: LogiView TTT", f"[{self.timestamp}] logiview_ttt.py started")
+
+            # Create globals
+            self.cnx = None
+            self.socket = None
+            self.cursorA = None
+            self.cursorB = None
+        except argparse.ArgumentError as e:
+            print(e)
+            self.logger.error(f"Error parsing command-line arguments: {e}")
+            if USE_PUSHBULLET:
+                self.pushbullet.push_note("ERROR: LogiView TTH",
+                                          f"[{self.timestamp}] Error parsing command-line arguments: {e}")
+        except Exception as e:
+            self.logger.error(f"Error during initialization: {e}")
+            if USE_PUSHBULLET:
+                self.pushbullet.push_note("ERROR: LogiView TTH",
+                                          f"[{self.timestamp}] Error during initialization: {e}")
+        else:
+            # Connect to the MySQL server
+            try:
+                self.cnx = mysql.connector.connect(
+                    user=args.user,
+                    password=args.password,
+                    host=args.host,
+                    database="logiview",  # Assuming you always connect to this database
+                )
+                self.logger.info("Successfully connected to the MySQL server!")
+                # Create a cursor to execute SQL statements.
+                self.cursorA = self.cnx.cursor(buffered=True)
+                self.cursorB = self.cnx.cursor(buffered=False)
+
+            except mysql.connector.Error as err:
+                if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
+                    self.logger.error("MySQL connection error: Incorrect username or password")
+                    if USE_PUSHBULLET:
+                        self.pushbullet.push_note("ERROR: LogiView TTH",
+                                                  f"[{self.timestamp}] MySQL connection error: Incorrect username or password")
+            else:
+                # Connect to Serial USB-Port
+                try:
+                    # Connect to socket
+                    self.sock = self.connect_to_socket(args.datahost, args.dataport)
+                    if self.sock is not None:
+                        self.logger.info(f"Opened socket to {args.datahost}")
+                except IOError as e:
+                    self.logger.error(f"I/O error({e.errno}): {e.strerror}")
+                    if USE_PUSHBULLET:
+                        self.pushbullet.push_note("ERROR: LogiView TTH", f"[{self.timestamp}] Unable to open ttyACM0! I/O error({e.errno}): {e.strerror}")
+                else:
+                    self.initialized = True  # Initialized OK!
+                    
+    def connect_to_socket(self, ip, port):
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((ip, port))
+            return sock
+        except socket.error as e:
+            self.logger.error(f"Socket error: {str(e)}")
+            if USE_PUSHBULLET:
+                self.pushbullet.push_note("ERROR: LogiView TTH", f"[{self.timestamp}] Socket connection error: {str(e)}")
+            return None
+        
+    def receive_data(self):
+        data = self.sock.recv(1024)
+        json_data = json.loads(data.decode())
+        return json_data
+
+    def setup_logging(self, logging_level=logging.WARNING):
+        try:
+            # Setting up the logging
+            logger = logging.getLogger('logiview_ttt')
+            logger.setLevel(logging_level)
+
+            # For syslog
+            syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+            syslog_format = logging.Formatter('%(name)s[%(process)d]: %(levelname)s - %(message)s')
+            syslog_handler.setFormatter(syslog_format)
+            logger.addHandler(syslog_handler)
+
+            # For console
+            console_handler = logging.StreamHandler()
+            console_format = logging.Formatter('%(levelname)s - %(message)s')
+            console_handler.setFormatter(console_format)
+            logger.addHandler(console_handler)
+
+            # Create an in-memory text stream to capture stderr
+            captured_output = io.StringIO()
+            self.original_stderr = sys.stderr
+            sys.stderr = captured_output  # Redirect stderr to captured_output
+
+            return logger
+        except Exception as e:
+            return None
+
+    def main_loop(self):
+        try:
+            while True:
+                if self.cursorA is None:
+                    self.logger.error(f"Database error: cursorA is None")
+                    sys.exit(1)
+                    
+                data = self.sock.recv(1024)
+            
+                try:
+                    sensor_data = json.loads(data.decode())
+                except json.JSONDecodeError as e:
+                    self.logger.error(f"Error decoding JSON: {e}")
+                    continue  # Skip processing this iteration
+                
+                self.logger.debug(f"SENSOR DATA: {sensor_data}")
+
+                DS18B20sqltxt = []
+
+                # Update Timestamp
+                self.timestamp = datetime.now().strftime("%y-%m-%d;%H:%M:%S")
+                
+                for sensor_value in sensor_data:
+                    sqlstr2 = f"SELECT * FROM logiview.sensors WHERE sensorscol = '{sensor_value['serial']}'"
+                    #print(sqlstr2)
+                    try:
+                        self.cursorA.execute(sqlstr2)
+                        sqldata = self.cursorA.fetchone()
+                        if sqldata:
+                            #print("Data found for serial:", sensor_value['serial'])
+                            DS18B20sqltxt.extend([str(int(float(sensor_value['temp']) * 100.0)), sqldata[4]])
+                        #else:
+                            #print("No data found for serial:", sensor_value['serial'])
+                    except mysql.connector.Error as err:
+                        print("Error executing SQL query:", err)
+                                                
+                #print(DS18B20sqltxt)
+
+                if DS18B20sqltxt:
+                    # Generate column names and values dynamically
+                    sensor_names = DS18B20sqltxt[1::2]  # Extract sensor names from DS18B20sqltxt
+                    columns = ', '.join(['datetime'] + [f'`{name}`' for name in sensor_names])
+
+                    # Ensure that there are enough values for all columns, fill missing values with 0
+                    values = [str(val) if val != ' ' else '0' for val in DS18B20sqltxt[::2]]
+                    values.insert(0, f"'{self.timestamp}'")
+                    values = ', '.join(values)
+
+                    sqlstr = f"INSERT INTO logiview.tempdata({columns}) VALUES ({values})"
+                    self.logger.debug(f"SQL: {sqlstr}")
+
+                    try:
+                        #print(sqlstr)
+                        self.cursorB.execute(sqlstr)
+                        self.cnx.commit()
+                    except mysql.connector.Error as err:
+                        sys.stdout.flush()
+                        self.logger.error("Error: {}".format(err))
+                        sys.stdout.flush()
+
+        except KeyboardInterrupt:
+            self.logger.info("Received a keyboard interrupt. Shutting down gracefully...")
+            self.cleanup()
+        except Exception as e:
+            self.logger.error(f"An unexpected error occurred: {e}")
+            if USE_PUSHBULLET:
+                self.pushbullet.push_note("ERROR: LogiView TTH", f"[{self.timestamp}] An unexpected error occurred: {e}")
+            sys.exit(1)
+
+    def cleanup(self):
+        if self.uart is not None:
+            try:
+                self.uart.close()
+            except Exception as e:
+                self.logger.error(f"Error closing UART: {e}")
+
+            if self.cnx is not None and self.cnx.is_connected():
+                self.cnx.close()
+
+
+def main():
+    try:
+        logiview_server = LogiviewTTHserver()   # Create TTH-Server
+        if logiview_server.initialized:
+            logiview_server.main_loop()             # if all ok then execute main loop
+        else:
+            logiview_server.pushbullet.push_note(
+                "ERROR: LogiView TTH", f"[{logiview_server.timestamp}] Initialize failed. Server not started!")
+            logiview_server.logger.error("Initialize failed. Server not started!")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    setproctitle.setproctitle('logiview_ttt')
+    main()
